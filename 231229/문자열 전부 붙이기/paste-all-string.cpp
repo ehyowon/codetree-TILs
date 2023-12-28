aa2f826d7d87ac277457abcdd3214ecb37a1d902
이효원 <129304827+ehyowon@users.noreply.github.com>
2023-12-29 00:22:29 +0900

    Title: 문자열 전부 붙이기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/문자열 전부 붙이기/paste-all-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string arr[10];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        cout << arr[i];
+    }
+    return 0;
+}
