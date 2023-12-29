9f1db4f404f29ea7b115fbc4edf2c41289b21cca
이효원 <129304827+ehyowon@users.noreply.github.com>
2023-12-30 00:24:18 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231230/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string A, B;
+    cin >> A >> B;
+    int n = 0, len = A.length();
+    for(int i = 0; i < len; i++){
+        A = A.substr(len-1, 1) + A.substr(0, len-1); 
+        n++;
+        if(A == B){
+            break;
+        }
+    }
+    if(n == len){
+        cout << -1;
+    }else{
+        cout << n;
+    }
+    return 0;
+}
