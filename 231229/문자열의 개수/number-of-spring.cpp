62cc38d2b9def84ee33f6afdf6fccd0c04c34eaf
이효원 <129304827+ehyowon@users.noreply.github.com>
2023-12-29 23:33:09 +0900

    Title: 문자열의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/문자열의 개수/number-of-spring.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str[201];
+	int cnt = 0;
+	while(true) {
+		cin >> str[cnt];
+		if(str[cnt] == "0"){
+			break;
+        }
+		cnt++;
+	}
+	cout << cnt << endl;
+	for(int i = 0; i < cnt; i += 2)
+		cout << str[i] << endl;
+    return 0;
+}
