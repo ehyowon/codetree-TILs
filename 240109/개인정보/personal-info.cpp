37873db403f489d15654f8bc56713cb20455f491
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-09 01:53:18 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/개인정보/personal-info.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class person{
+    public:
+        string name;
+        int height;
+        double weight;
+        person(string name, int height, double weight){
+            this->name = name;
+            this->height = height;
+            this->weight = weight;
+        }
+        person(){}
+};
+
+bool cmp_n(person a, person b){
+    return a.name < b.name;
+}
+
+bool cmp_h(person a, person b){
+    return a.height > b.height;
+}
+
+person people[5];
+
+int main() {
+    for(int i = 0; i < 5; i++){
+        string name;
+        int height;
+        double weight;
+        cin >> name >> height >> weight;
+        people[i] = person(name, height, weight);
+    }
+    sort(people, people + 5, cmp_n);
+    cout << "name" << endl;
+    for(int i = 0; i < 5; i++){
+        cout << people[i].name << " " << people[i].height << " " << people[i].weight << endl;
+    }
+    cout << endl;
+    sort(people, people + 5, cmp_h);
+    cout << "height" << endl;
+    for(int i = 0; i < 5; i++){
+        cout << people[i].name << " " << people[i].height << " " << people[i].weight << endl;
+    }
+    return 0;
+}
