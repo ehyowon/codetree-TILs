0da18aa493e42aeaa0d896dd40db905d66f70f7f
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-08 22:00:13 +0900

    Title: 코드네임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/코드네임/code-name.cpp
+++ 240108/코드네임/code-name.cpp
@@ -6,11 +6,12 @@ class Code{
     public:
         char name;
         int score;
-    Code(char name, int score){
-        this->name = name;
-        this->score = score;
-    }
-}
+        Code(char name, int score){
+            this->name = name;
+            this->score = score;
+        }
+        Code(){}
+};
 
 Code agents[5];
 
@@ -19,9 +20,11 @@ int main() {
         char name;
         int score;
         cin >> name >> score;
+
+        agents[i] = Code(name, score);
     }
     int min = 0;
-    for(int i = 0; i < 5; i++){
+    for(int i = 1; i < 5; i++){
         if(agents[min].score > agents[i].score){
             min = i;
         }
