34d0fd5cf44a7739e36245515823c80b10a91818
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-10 03:27:09 +0900

    Title: 여러가지 진수변환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, b;
+    cin >> n >> b;
+    int digits[100] = {};
+    int cnt = 0;
+    while(true){
+        if(n < b){
+            digits[cnt++] = n;
+            break;
+        }
+        digits[cnt++] = n % b;
+        n /= b;
+    }
+    for(int i = cnt - 1; i >= 0; i--){
+        cout << digits[i];
+    }
+    return 0;
+}
