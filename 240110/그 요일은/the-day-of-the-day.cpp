bb9dcab03d7962cab5ca05581df2591df9da981c
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-10 02:48:53 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240110/그 요일은/the-day-of-the-day.cpp
+++ 240110/그 요일은/the-day-of-the-day.cpp
@@ -7,21 +7,21 @@ int main() {
     string A;
     cin >> m1 >> d1 >> m2 >> d2 >> A;
     int month = m1, day = d1;
-    int days;
+    int days = 1;
     if(A == "Sun"){
-        days = 0;
+        days += 0;
     }else if(A == "Mon"){
-        days = 1;
+        days += 7;
     }else if(A == "Tue"){
-        days = 2;
+        days += 1;
     }else if(A == "Wed"){
-        days = 3;
+        days += 2;
     }else if(A == "Thu"){
-        days = 4;
+        days += 3;
     }else if(A == "Fri"){
-        days = 5;
+        days += 4;
     }else if(A == "Sat"){
-        days = 6;
+        days += 5;
     }
     int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     while(true) {
