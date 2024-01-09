b848ba1613c688d75132833bac1e936447463b74
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-10 03:31:42 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    string n;
+    cin >> a >> b >> n;
+    
+    int num = 0;
+    for(int i = 0; i < (int) n.size(); i++){
+        num = num * a + (n[i] - '0');
+    }
+
+    int digits[100] = {};
+    int cnt = 0;
+    while(true){
+        if(num < b){
+            digits[cnt++] = num;
+            break;
+        }
+        digits[cnt++] = num % b;
+        num /= b;
+    }
+    for(int i = cnt - 1; i >= 0; i--){
+        cout << digits[i];
+    }
+    return 0;
+}
