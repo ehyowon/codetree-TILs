2dd0d17c1605aa1fcab7040a76535c60580f76cd
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-10 02:03:12 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240110/DateTime to DateTime/datetime-to-datetime.cpp
@@ -6,25 +6,26 @@ int main() {
     cin >> a >> b >> c;
     int day = 11, hour = 11, min = 11;
     int minute = 0;
-    while(true){
-        if(day == a && hour == b && min == c){
-            break;
-        }
-        minute++;
-        min++;
-        if(min == 60){
-            hour++;
-            min = 0;
-        }
-        if(hour == 24){
-            day++;
-            hour = 0;
-        }
-    }
     if(a < 11 || (a == 11 && b < 11) || (a == 11 && b == 11 && c < 11)){
         cout << -1;
     }else{
+        while(true){
+            if(day == a && hour == b && min == c){
+                break;
+            }
+            minute++;
+            min++;
+            if(min == 60){
+                hour++;
+                min = 0;
+            }
+            if(hour == 24){
+                day++;
+                hour = 0;
+            }
+        }
         cout << minute;
     }
+    
     return 0;
 }
