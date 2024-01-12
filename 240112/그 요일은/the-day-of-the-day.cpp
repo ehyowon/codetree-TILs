f4e8cf8a8f0bd3cb6c3f9c39488585027acaefae
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-12 20:08:23 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240112/그 요일은/the-day-of-the-day.cpp
+++ 240112/그 요일은/the-day-of-the-day.cpp
@@ -6,35 +6,38 @@ int main() {
     int m1, m2, d1, d2;
     string A;
     cin >> m1 >> d1 >> m2 >> d2 >> A;
-    int month = m1, day = d1;
-    int days = 1;
-    if(A == "Sun"){
-        days += 6;
-    }else if(A == "Mon"){
-        days += 7;
-    }else if(A == "Tue"){
-        days += 1;
-    }else if(A == "Wed"){
-        days += 2;
-    }else if(A == "Thu"){
-        days += 3;
-    }else if(A == "Fri"){
-        days += 4;
-    }else if(A == "Sat"){
-        days += 5;
-    }
+    int month = m1, day = d1, i = 0;
+    int week[7] = {};
     int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    while(true) {
+    while(true){
         if(month == m2 && day == d2){
             break;
         }
-        days++;
         day++;
+        week[i]++;
+        i++;
         if(day > num_of_days[month]){
             month++;
             day = 1;
         }
+        if(i >= 7){
+            i = 0;
+        }
     }
-    cout << days / 7;
+    if(A == "Sun"){
+        cout << week[6];
+    }else if(A == "Mon"){
+        cout << week[0];
+    }else if(A == "Tue"){
+        cout << week[1];
+    }else if(A == "Wed"){
+        cout << week[2];
+    }else if(A == "Thu"){
+        cout << week[3];
+    }else if(A == "Fri"){
+        cout << week[4];
+    }else if(A == "Sat"){
+        cout << week[5];
+    }
     return 0;
 }
