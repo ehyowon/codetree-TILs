6c6a77d3287ebfbcedc38ff60de3648f0b52e341
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-18 16:43:36 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a[1000];
+    bool Plus[1000] = {};
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int cnt = 1, maxcnt = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] < 0){
+            Plus[i] = false;
+        }else{
+            Plus[i] = true;
+        }
+
+        if(Plus[i] == Plus[i-1]){
+            cnt++;
+        }else{
+            cnt = 1;
+        }
+
+        if(cnt > maxcnt){
+            maxcnt = cnt;
+        }
+    }
+    cout << maxcnt;
+    return 0;
+}
