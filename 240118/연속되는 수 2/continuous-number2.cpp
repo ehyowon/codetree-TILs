a483eb0f9440af395e5309f9008c15bfc3639fd3
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-18 16:34:39 +0900

    Title: 연속되는 수 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/연속되는 수 2/continuous-number2.cpp
+++ 240118/연속되는 수 2/continuous-number2.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
-    int n, a[1000] = {}, cnt = 0, maxcnt = 0;
+    int n, a[1000] = {}, cnt = 1, maxcnt = 0;
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
     for(int i = 0; i < n; i++){
-        if(i == 0 || a[i] != a[i - 1]){
-               cnt++;
+        if(a[i] == a[i - 1]){
+            cnt++;
+        }else{
+            cnt = 1;
         }
         if(cnt > maxcnt){
             maxcnt = cnt;
