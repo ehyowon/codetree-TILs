f6eb6fe9974fabd0f2983bc67768a886bca72d35
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-18 18:56:05 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/벌금은 누구에게/who-will-pay.cpp
+++ 240118/벌금은 누구에게/who-will-pay.cpp
@@ -2,21 +2,45 @@
 using namespace std;
 
 int main() {
-    int n, m, k, num[10000];
+    int n, m, k, num[10000], l;
     cin >> n >> m >> k;
-    for(int i = 0; i < m; i++){
-        cin >> num[i];
-    }
     bool Pay = false;
-    for(int i = 0; i < m; i++){
-        if(num[i] > k){
-            Pay = true;
-            cout << i+1;
+    for(int i = 1; i <= m; i++){
+        cin >> l;
+        num[l]++;
+        for(int j = 1; j <= n; j++){
+            if(num[j] >= k){
+                Pay = true;
+                cout << j;
+                break;
+            }
+        }
+        if(Pay == true){
             break;
         }
     }
     if(Pay == false){
         cout << -1;
     }
+
+    
+    /*int stu[n] = {};
+    bool Pay = false;
+    for(int i = 0; i < m; i++){
+        stu[num[i]]++;
+        for(int j = 1; j <= n; j++){
+            if(stu[j] >= k){
+                Pay = true;
+                cout << j;
+                break;
+            }
+        }
+        if(Pay == true){
+            break;
+        }
+    }
+    if(Pay == false){
+        cout << -1;
+    }*/
     return 0;
 }
