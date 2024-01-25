51a98f7afe1a7522189aaf38f20ca3d058575394
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-25 17:27:34 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240125/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 240125/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    int n, m, r[10001], c[10001];
+    int n, m, r[10000], c[10000];
     cin >> n >> m;
     int area[n][n] = {};
-    for(int i = 1; i <= m; i++){
+    for(int i = 0; i < m; i++){
         cin >> r[i] >> c[i];
         int r0 = r[i]-1, c0 = c[i]-1;
         area[r0][c0] = 1;
