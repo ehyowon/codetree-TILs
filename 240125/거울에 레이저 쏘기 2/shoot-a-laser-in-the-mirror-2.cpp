9af3531add62fe1216487f48041b01eee460afd9
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-01-25 20:07:14 +0900

    Title: 거울에 레이저 쏘기 2, Time: 8ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 240125/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240125/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -34,15 +34,21 @@ int main() {
     }else if(k > n && k <= 2*n){ //왼쪽으로
         dir = 3;
         y = n - 1;
-        x = k % 4 - 1;
+        x = k % 4 - n - 1;
     }else if(k > 2*n && k <= 3*n){ //위로
         dir = 2;
         x = n - 1;
-        y = k % 4 - 1;
+        y = n - k % 4;
     }else if(k > 3*n && k <= 4*n){ //오른쪽으로
-        dir = 0;
+        dir = 0; 
         y = 0;
-        x = k % 4 - 1;
+        x = k % 4 - n;
+    }
+
+    if(x < 0){
+        x += n;
+    }else if(y < 0){
+        y += n;
     }
 
     int cnt = 0;
